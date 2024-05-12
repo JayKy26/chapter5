02f41efef31bebc348080d589e6395fea30e47c6
LAPTOP-156OJCEG\nguye <nguyendaiky2003@gmail.com>
2024-05-12 23:28:01 +0700

    Chapter5

---
+++ Implement_Factories_Method/Implement_Factories_Method/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_Factories_Pattern
+{
+    
+    public interface IAnimal
+    {
+        void AboutMe(); // mô tả thông tin của đối tượng động vật
+    }
+
+   
+    public class Lion : IAnimal
+    {
+        public void AboutMe() => Console.WriteLine("This is Lion."); // hiển thị thông tin Lion
+    }
+
+    public class Tiger : IAnimal
+    {
+        public void AboutMe() => Console.WriteLine("This is Tiger."); // hiển thị thông tin Tiger
+    }
+
+    
+    public abstract class AnimalFactory
+    {
+        public abstract IAnimal CreateAnimal(); //tạo ra một đối tượng
+    }
+
+    // triển khai AnimalFactory tạo ra các đối tượng lion
+    public class LionFactory : AnimalFactory
+    {
+        public override IAnimal CreateAnimal() => new Lion(); // Tạo và trả về một đối tượng mới
+    }
+
+   
+    public class TigerFactory : AnimalFactory
+    {
+        public override IAnimal CreateAnimal() => new Tiger(); 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Factory Method Pattern Demo. ***\n");
+
+            // tạo một danh sách AnimalFactory
+            List<AnimalFactory> animalFactoryList = new List<AnimalFactory>
+            {
+                new TigerFactory(), // thêm vào danh sách 
+                new LionFactory()   
+            };
+
+            // duyệt từng animal và tạo ra mỗi cái một factory
+            foreach (var animalFactory in animalFactoryList)
+            {
+                var animal = animalFactory.CreateAnimal(); // taooj ra một đối tượng Animal
+                animal.AboutMe(); // hiển thị thong tin
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Implement_Singleton_Pattern/Implement_Singleton_Pattern/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public sealed class Singleton
+{
+    // lưu trữ thể hiện duy nhất
+    private static readonly Singleton Instance;
+
+    // tổng số thể hiệnđược tạo
+    private static int TotalInstances = 0;
+
+
+    private Singleton()
+    {
+        Console.WriteLine("Private constructor is call");
+    }
+
+    
+    static Singleton()
+    {
+        Console.WriteLine("Static constructor is called.");
+        Instance = new Singleton();
+        TotalInstances++;
+        Console.WriteLine($"Singleton instance is created. Number of instances: {TotalInstances}");
+        Console.WriteLine("Exit from static constructor");
+    }
+
+    // lấy thể hiện duy nhất của lớp
+    public static Singleton GetInstance => Instance;
+
+    //  tổng số thể hiện đã được tạo ra
+    public int GetTotalInstances => TotalInstances;
+
+   
+    public void Print() => Console.WriteLine("Heloo");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("#1. Trying to get a Singleton instance, called firstInstance.");
+  
+        Singleton firstInstance = Singleton.GetInstance;
+        Console.WriteLine("Invoke Print() method:");
+       
+        firstInstance.Print();
+
+        Console.WriteLine("#2. Trying to get another Singleton instance, called secondInstance.");
+      
+        Singleton secondInstance = Singleton.GetInstance;
+        Console.WriteLine($"Number of instances: {secondInstance.GetTotalInstances}");
+        Console.WriteLine("Invoke Print() method:");
+        
+        secondInstance.Print();
+
+        // Check ca 2tham chiếu có trỏ đến cùng thể hiện không
+        if (firstInstance.Equals(secondInstance))
+        {
+            Console.WriteLine("=> The firstInstance and secondInstance are the same.");
+        }
+        else
+        {
+            Console.WriteLine("=> Different instance exists.");
+        }
+
+        Console.Read();
+    }
+}
